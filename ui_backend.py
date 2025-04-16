3fcba1e312c06ca93f248d5c890da2adc803868a
alice <alicemager006@gmail.com>
2025-04-16 16:26:27 +0300

    add route to update file permissions from UI and forward changes to agent

--- ui_backend.py
+++ ui_backend.py
@@ -26,7 +26,7 @@ def register():
     print(register_data.encode())
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(("192.168.71.128", 2512))
+        sock.connect(("192.168.20.1", 2512))
 
         sock.send(register_data.encode())
         resp = sock.recv(1024)
@@ -36,5 +36,35 @@ def register():
         print(e)
 
     return "{\"FUCK\":\"YOU\"}"
+
+
+@app.route("/update-permissions", methods=["POST"])
+def update_permissions():
+    file_data = request.json
+
+    try:
+        register_data = ""
+        for user in file_data['sharedWith']:
+            register_data += f"{user['name']};{'\x00' if user['access'] == 'read' else '\x01'}|"
+        register_data = register_data[:-1]  
+
+        register_data = f"\x02C:\\Users\\Rick\\Documents\\DT\\{file_data['name']}$" + register_data + "$"
+
+        print(register_data.encode())  
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("192.168.20.1", 2512))
+
+        sock.send(register_data.encode())
+        resp = sock.recv(1024)
+        print(resp)
+
+    except Exception as e:
+        print("Error sending permissions update:")
+        print(e)
+        return {"status": "fail", "error": str(e)}, 500
+
+    return {"status": "success"}
+
 
 app.run(host="0.0.0.0", port="2513", debug=True)
