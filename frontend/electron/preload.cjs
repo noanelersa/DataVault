d93877e1e7b3b0957a173f654b03ad4f4524d8f3
noanelersa <noa.nelersa@gmail.com>
2025-04-19 18:00:50 +0300

    Reworked file upload flow to send data to Electron agent, handle its response, and update UI accordingly, in FileUploadForm after encounter some issues with the previous approach. also designed the electron's main so other buttons flow could be added in the future. removed the mocker i did earlier.

--- frontend/electron/preload.cjs
+++ frontend/electron/preload.cjs
@@ -1,8 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('agentAPI', {
-  sendCommand: async (command) => {
-    const response = await ipcRenderer.invoke('agent-command', command);
-    return response;
-  },
+  sendCommand: (command, args) => ipcRenderer.invoke('agent-command', command, args),
 });
