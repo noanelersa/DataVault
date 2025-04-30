cac5ea2eb2c4773bfae7229fb829f66f6436368a
Talia <taliamoshe06@cs.colman.ac.il>
2025-04-30 17:45:25 +0300

    initial  alerts page setup with mock

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
 import FileUploadPage from './pages/FileUploadForm';
+import AlertsPage from './pages/AlertsPage'; // ייבוא עמוד ההתרעות
+import { useState } from 'react';
+
 function App() {
+  const [currentTab, setCurrentTab] = useState('files'); // מצב שמנהל איזו לשונית מוצגת
+
   return (
-    <FileUploadPage></FileUploadPage>
+      <div>
+        <div className="tabs">
+          <button onClick={() => setCurrentTab('files')}>Files</button>
+          <button onClick={() => setCurrentTab('alerts')}>Alerts</button>
+        </div>
+
+        {currentTab === 'files' && <FileUploadPage />}
+        {currentTab === 'alerts' && <AlertsPage />}
+      </div>
   )
-  
 }
-export default App
+
+export default App;
