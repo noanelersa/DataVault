e1193f6b94361420cd2a1810ea568446db485bb9
alice <alicemager006@gmail.com>
2025-05-04 16:03:45 +0300

    Implement a simple login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginPage from './LoginPage';
 import FileManagementSystem from './FileManagementSystem';
 
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+
+  const handleLogin = (username: string) => {
+    setIsLoggedIn(true);
+    setUsername(username); 
+  };
+
   return (
-    <div className="p-6">
-      <FileManagementSystem />
+    <div className="p-0">
+      {isLoggedIn ? (
+        <FileManagementSystem username={username} />
+      ) : (
+        <LoginPage onLogin={handleLogin} />
+      )}
     </div>
   );
 }
