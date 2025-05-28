967144741beea95176bb629b5f54d8f8e14a0e4e
Talia <taliamoshe06@cs.colman.ac.il>
2025-05-28 22:14:00 +0300

    changed api route to only /acl

--- server/src/main/java/datavault/server/controllers/AclController.java
+++ server/src/main/java/datavault/server/controllers/AclController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/acl")
+@RequestMapping("/acl")
 public class AclController {
     @Autowired
     private AclService aclService;
